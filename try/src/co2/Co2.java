41486f58b58a1d59a9dcf0be7b859f87a5d7f3e4
k40211127 <30898781+k40211127@users.noreply.github.com>
2017-09-21 11:49:59 +0800

    Update Co2.java

--- try/src/co2/Co2.java
+++ try/src/co2/Co2.java
@@ -12,31 +12,31 @@ public class Co2 {
 			count += Process[i];  hour = count/60;	 min = count%60;
 			if (count > 1440) {System.out.println("超過24小時");break;}
 			else if (count >=   0 && count < 420) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Off-peak區段");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Off-peak區段";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Off-peak區段";
 			}	
 			else if (time <420 && count>=420) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到7:00是 Off-peak區間,"+"7:00到"+count/60+":"+count%60+"是Mid-peak區間");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到7:00是 Off-peak區間,"+"7:00到"+count/60+":"+count%60+"是Mid-peak區間";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到7:00是 Off-peak區間,"+"7:00到"+count/60+":"+count%60+"是Mid-peak區間";
 			}
 			else if (count >= 420 && count < 900) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Mid-peak區段");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Mid-peak區段";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Mid-peak區段";
 			}
 			else if (time <900 && count>=900) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到15:00是 Mid-peak區間,"+"15:00到"+count/60+":"+count%60+"是On-peak區間");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到15:00是 Mid-peak區間,"+"15:00到"+count/60+":"+count%60+"是On-peak區間";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到15:00是 Mid-peak區間,"+"15:00到"+count/60+":"+count%60+"是On-peak區間";
 			}
 			else if (count >= 900 && count <1200) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"On -peak區段");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"On -peak區段";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"On -peak區段";
 			}
 			else if (time <1200 && count>=1200) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到20:00是 On-peak區間,"+"20:00到"+count/60+":"+count%60+"是Mid-peak區間");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到20:00是 On-peak區間,"+"20:00到"+count/60+":"+count%60+"是Mid-peak區間";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到20:00是 On-peak區間,"+"20:00到"+count/60+":"+count%60+"是Mid-peak區間";
 			}
 			else if (count >=1200 && count <1320) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Mid-peak區段");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Mid-peak區段";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Mid-peak區段";
 			}
 			else if (time <1320 && count>=1320) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到22:00是 Mid-peak區間,"+"22:00到"+count/60+":"+count%60+"是Off-peak區間");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到22:00是 Mid-peak區間,"+"22:00到"+count/60+":"+count%60+"是Off-peak區間";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘,其中"+hour+":"+min+"到22:00是 Mid-peak區間,"+"22:00到"+count/60+":"+count%60+"是Off-peak區間";
 			}
 			else if (count >=1320 && count <=1440) {System.out.println("開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Off-peak區段");
-			TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Off-peak區段";
+				TimeRecord[time/60][time%60][hour][min] = "開始時間："+time/60+"："+time%60+"\t"+"處理了"+Process[i]+"分鐘後"+"目前時間為 "+hour+":"+min+" 落在"+"Off-peak區段";
 			}
 		}
 		//    7    15  20 22
