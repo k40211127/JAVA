7f3e02fe0b3e95d79aa7baf097d14ddcf14a7707
k40211127 <30898781+k40211127@users.noreply.github.com>
2017-09-28 20:45:54 +0800

    Update Co2.java

--- try/src/co2/Co2.java
+++ try/src/co2/Co2.java
@@ -1,61 +1,187 @@
 package co2;
-
+import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.xml.crypto.Data;
+
 public class Co2 {
-	//public Co2(int Process[]) {}
-	public static void main(String[] argsp) throws ParseException {
+	public static void main(String[] args) throws ParseException {		 
+		SimpleDateFormat Time = new SimpleDateFormat("HH:mm");
+		int count = 120,h,m,time,H,M;
+		int[] Process = {50,20,80,60,50,65,89,62,40,65,89,77,91,54,69,71,26,12,80,90,21,40,18,1,1,80,80,80,80};
 		
-		SimpleDateFormat simple = new SimpleDateFormat("HH:mm");
-		 
-		String TimeRecord[][][][] = new String[24][60][25][60];
-		int[] Process = {50,20,80,60,50,100,89,62,40,65,89,77,91,50,60,70,26,1};
-		//int[] Process = {50,20,80,60,50,99,89,99,87,50,60,70,80,50};
-		//int[] Power = {20,30,15,10,15,30,10,20};
-		int count = 360,hour = count/60,min = count%60;
-		
+		Date h3 = Time.parse("03:00"),h6 = Time.parse("06:00"),h12 = Time.parse("12:00"),
+			 h14 = Time.parse("14:00"),h17 = Time.parse("17:00"),h18 = Time.parse("18:00"),
+			 h21 = Time.parse("21:00"),h23 = Time.parse("23:00"),h24 = Time.parse("24:00");
+			//   0~3   3~6  6~12 12~14 14~17
+			// 23~24 21~23 18~21 17~18
+			// 0.725 0.700 0.693 0.682 0.669
+		String hh = "";
 		for (int i=0;i<Process.length;i++) {
-			int time = count;	
-			count += Process[i];  hour = count/60;	 min = count%60; 
-			String sT = time/60 +":"+ time%60 , nT = hour+":"+min;
-			
-			int a = (int) (simple.parse(sT).getTime()/(60000));
-			String startTime = "開始時間："+simple.format(a*60000)+"分,處理了"+Process[i]+"分鐘後,";
-			
-			Date nt = simple.parse(nT),st = simple.parse(sT);
-			String nowtime="目前時間為 "+simple.format(nt);	
+			time = count;H=time/60;M=time%60;
+			count +=Process[i]; h=count/60; m=count%60;
+			hh += h+" ";
+			if (h >= 24 || H >=24) {while(h>=24) {h-=24;} while(H>=24) {H-=24;}hh += h+" ";}
+			Date startTime = Time.parse(H+":"+M);//System.out.println(Time.format(startTime));
+			Date endTime  = Time.parse(h+":"+m); //System.out.println(Time.format(endTime));
 			
 			
-			if (count >= 0 && count <420){TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Off-peak區間";}
-				else if (time  < 420 && count >= 420 && count <900){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 420 && count >  900 && count<1200){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15:00到"+simple.format(nt)+"分是A區間";}
-				else if (time  < 420 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15:00到20:00是A區間,20:00到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 420 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15:00到20:00是A區間,20:00到22:00是B區間,22:00到"+simple.format(nt)+"分是C區間";}	
-			else if (count >=420 && count <  900) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Mid-peak區間";}
-				else if (time  < 900 && count >  900 && count<1200){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到"+simple.format(nt)+"分是A區間";}
-				else if (time  < 900 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到20:00是A區間,20:00到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 900 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到20:00是A區間,20:00到22:00是B區間,22:00到"+simple.format(nt)+"分是C區間";}			  
-			else if (count >=900 && count < 1200) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"On -peak區間";}	
-				else if (time  < 1200 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到20:00是 A區間,"+"20:00到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 1200 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到20:00是 A區間,"+"20:00到22:00是B區間,22:00到"+simple.format(nt)+"分是C區間";}
-			else if (count >=1200 && count< 1320) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Mid-peak區間";}
-				else if (time  < 1320 && count>=1320 &&count<=1440) {TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到22:00是 Mid-peak區間,"+"22:00到"+simple.format(nt)+"是Off-peak區間";}
-			else if (count >=1320 && count< 1440) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Off-peak區間";}
-		}	
-		//    7    15  20 22
-		// 0 4 8 12 16 20 24
-		
-		for (int i=0;i<24;i++) {
-			for (int j=0;j<60;j++) {
-				for (int ii=0;ii<25;ii++) {
-					for (int jj=0;jj<60;jj++) {
-						if (TimeRecord[i][j][ii][jj]!=null) {System.out.println(TimeRecord[i][j][ii][jj]);}
-					}
-				}
+			
+			double sum =0;
+			switch (H) {
+			case 0:case 1:case 2:
+				if (startTime.before(h3) && endTime.before(h3) || endTime.equals(h3)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" S");	break;}
+				switch (h) {
+					case 3:case 4:case 5:case 6:
+						if (startTime.before(h3) && endTime.before(h6) || endTime.equals(h6)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to "+Time.format(endTime)+" A");	break;}
+					case 7:case 8:case 9:case 10:case 11:
+						if (startTime.before(h3) && endTime.before(h12) || endTime.equals(h12)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to "+Time.format(endTime)+" B");	break;}
+					case 12:case 13:
+						if (startTime.before(h3) && endTime.before(h14) || endTime.equals(h14)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to 12:00 B, 12:00 to "+Time.format(endTime)+" C");	break;}
+					case 14:case 15:case 16:
+						if (startTime.before(h3) && endTime.before(h17) || endTime.equals(h17)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to "+Time.format(endTime)+" D");	break;}
+					case 17:
+						if (startTime.before(h3) && endTime.before(h18) || endTime.equals(h18)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to "+Time.format(endTime)+" C");	break;}
+					case 18:case 19:case 20:
+						if (startTime.before(h3) && endTime.before(h21) || endTime.equals(h21)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to "+Time.format(endTime)+" B");	break;}
+					case 21:case 22:
+						if (startTime.before(h3) && endTime.before(h23) || endTime.equals(h23)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}
+					case 23:
+						if (startTime.before(h3) && endTime.before(h24) || endTime.equals(h24)) {
+							System.out.println(Time.format(startTime)+" to 03:00 S, 03:00 to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;
+			case 3:case 4:case 5:
+				if (startTime.before(h6) && endTime.before(h6) || endTime.equals(h6)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" A");	break;}
+				switch (h) {	
+					case 6:case 7:case 8:case 9:case 10:case 11:
+						if (startTime.before(h6) && endTime.before(h12) || endTime.equals(h12)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to "+Time.format(endTime)+" B");	break;}
+					case 12:case 13:
+						if (startTime.before(h6) && endTime.before(h14) || endTime.equals(h14)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to 12:00 B, 12:00 to "+Time.format(endTime)+" C");	break;}
+					case 14:case 15:case 16:
+						if (startTime.before(h6) && endTime.before(h17) || endTime.equals(h17)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to "+Time.format(endTime)+" D");	break;}
+					case 17:
+						if (startTime.before(h6) && endTime.before(h18) || endTime.equals(h18)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to "+Time.format(endTime)+" C");	break;}
+					case 18:case 19:case 20:
+						if (startTime.before(h6) && endTime.before(h21) || endTime.equals(h21)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to "+Time.format(endTime)+" B");	break;}
+					case 21:case 22:
+						if (startTime.before(h6) && endTime.before(h23) || endTime.equals(h23)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}	
+					case 23:
+						if (startTime.before(h6) && endTime.before(h24) || endTime.equals(h24)) {
+							System.out.println(Time.format(startTime)+" to 06:00 A, 06:00 to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;
+			case 6:case 7:case 8:case 9:case 10:case 11:
+				if (startTime.before(h12) && endTime.before(h12) || endTime.equals(h12)) { 
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" B");	break;}
+				switch (h) {
+				case 12:case 13:
+					if (startTime.before(h12) && endTime.before(h14) || endTime.equals(h14)) {
+						System.out.println(Time.format(startTime)+" to 12:00 B, 12:00 to "+Time.format(endTime)+" C");	break;}
+				case 14:case 15:case 16:
+					if (startTime.before(h12) && endTime.before(h17) || endTime.equals(h17)) {
+						System.out.println(Time.format(startTime)+" to 12:00 B, 12:00 to 14:00 C, 14:00 to "+Time.format(endTime)+" D");	break;}
+				case 17:
+					if (startTime.before(h12) && endTime.before(h18) || endTime.equals(h18)) {
+						System.out.println(Time.format(startTime)+" to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to "+Time.format(endTime)+" C");	break;}
+				case 18:case 19:case 20:
+					if (startTime.before(h12) && endTime.before(h21) || endTime.equals(h21)) {
+						System.out.println(Time.format(startTime)+" to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to "+Time.format(endTime)+" B");	break;}
+				case 21:case 22:
+					if (startTime.before(h12) && endTime.before(h23) || endTime.equals(h23)) {
+						System.out.println(Time.format(startTime)+" to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}
+				case 23:
+					if (startTime.before(h12) && endTime.before(h24) || endTime.equals(h24)) {
+						System.out.println(Time.format(startTime)+" to 12:00 B, 12:00 to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;
+			case 12:case 13:
+				if (startTime.before(h14) && endTime.before(h14) || endTime.equals(h14)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" C");	break;}
+				switch (h) {			
+					case 14:case 15:case 16:
+						if (startTime.before(h14) && endTime.before(h17) || endTime.equals(h17)) {
+							System.out.println(Time.format(startTime)+" to 14:00 C, 14:00 to "+Time.format(endTime)+" D");	break;}
+					case 17:
+						if (startTime.before(h14) && endTime.before(h18) || endTime.equals(h18)) {
+							System.out.println(Time.format(startTime)+" to 14:00 C, 14:00 to 17:00 D, 17:00 to "+Time.format(endTime)+" C");	break;}
+					case 18:case 19:case 20:
+						if (startTime.before(h14) && endTime.before(h21) || endTime.equals(h21)) {
+							System.out.println(Time.format(startTime)+" to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to "+Time.format(endTime)+" B");	break;}
+					case 21:case 22:
+						if (startTime.before(h14) && endTime.before(h23) || endTime.equals(h23)) {
+							System.out.println(Time.format(startTime)+" to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}
+					case 23:
+						if (startTime.before(h14) && endTime.before(h24) || endTime.equals(h24)) {
+							System.out.println(Time.format(startTime)+" to 14:00 C, 14:00 to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;
+			case 14:case 15:case 16:	
+				if (startTime.before(h17) && endTime.before(h17) || endTime.equals(h17)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" D");	break;}
+				switch (h) {	
+					case 17:
+						if (startTime.before(h17) && endTime.before(h18) || endTime.equals(h18)) {
+							System.out.println(Time.format(startTime)+" to 17:00 D, 17:00 to "+Time.format(endTime)+" C");	break;}
+					case 18:case 19:case 20:
+						if (startTime.before(h17) && endTime.before(h21) || endTime.equals(h21)) {
+							System.out.println(Time.format(startTime)+" to 17:00 D, 17:00 to 18:00 C, 18:00 to "+Time.format(endTime)+" B");	break;}
+					case 21:case 22:
+						if (startTime.before(h17) && endTime.before(h23) || endTime.equals(h23)) {
+							System.out.println(Time.format(startTime)+" to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}
+					case 23:
+						if (startTime.before(h17) && endTime.before(h24) || endTime.equals(h24)) {
+							System.out.println(Time.format(startTime)+" to 17:00 D, 17:00 to 18:00 C, 18:00 to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;	
+			case 17:
+				if (startTime.before(h18) && endTime.before(h18) || endTime.equals(h18)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" C");	break;}
+				switch (h) {
+				case 18:case 19:case 20:
+					if (startTime.before(h18) && endTime.before(h21) || endTime.equals(h21)) {
+						System.out.println(Time.format(startTime)+" to 18:00 C, 18:00 to "+Time.format(endTime)+" B");	break;}
+				case 21:case 22:
+					if (startTime.before(h18) && endTime.before(h23) || endTime.equals(h23)) {
+						System.out.println(Time.format(startTime)+" to 18:00 C, 18:00 to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}
+				case 23:
+					if (startTime.before(h18) && endTime.before(h24) || endTime.equals(h24)) {
+						System.out.println(Time.format(startTime)+" to 18:00 C, 18:00 to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;
+			case 18:case 19:case 20:
+				if (startTime.before(h21) && endTime.before(h21) || endTime.equals(h21)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" B");	break;}
+				switch (h) {		
+					case 21:case 22:
+						if (startTime.before(h21) && endTime.before(h23) || endTime.equals(h23)) {
+							System.out.println(Time.format(startTime)+" to 21:00 B, 21:00 to "+Time.format(endTime)+" A");	break;}
+					case 23:
+						if (startTime.before(h21) && endTime.before(h24) || endTime.equals(h24)) {
+							System.out.println(Time.format(startTime)+" to 21:00 B, 21:00 to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+				}break;
+			case 21:case 22:
+				if (startTime.before(h23) && endTime.before(h23) || endTime.equals(h23)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+" A");	break;}
+				if (startTime.before(h23) && endTime.before(h24) || endTime.equals(h24)) {
+					System.out.println(Time.format(startTime)+" to 23:00 A, 23:00 to "+Time.format(endTime)+" S");	break;}
+			case 23:
+				if (startTime.before(h24) && endTime.before(h24) || endTime.equals(h24)) {
+					System.out.println(Time.format(startTime)+" to "+Time.format(endTime)+ " S");	break;}
 			}
 		}
-
+			//float mm = 40f/100f;
+			//System.out.println(Math.rint((mm*100)/100));
 	}
 }
