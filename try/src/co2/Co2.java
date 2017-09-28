870ab941f0b709fe6834dfc0b8862a6a5f7d61da
k40211127 <30898781+k40211127@users.noreply.github.com>
2017-09-28 12:48:50 +0800

    Update Co2.java

--- try/src/co2/Co2.java
+++ try/src/co2/Co2.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Te {
+public class Co2 {
 	//public Co2(int Process[]) {}
 	public static void main(String[] argsp) throws ParseException {
 		
@@ -30,16 +30,16 @@ public class Te {
 			
 			if (count >= 0 && count <420){TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Off-peak區間";}
 				else if (time  < 420 && count >= 420 && count <900){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 420 && count >  900 && count<1200){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15點到"+simple.format(nt)+"分是A區間";}
-				else if (time  < 420 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15點到20點是A區間,20點到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 420 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15點到20點是A區間,20點到22點是B區間,22點到"+simple.format(nt)+"分是C區間";}	
+				else if (time  < 420 && count >  900 && count<1200){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15:00到"+simple.format(nt)+"分是A區間";}
+				else if (time  < 420 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15:00到20:00是A區間,20:00到"+simple.format(nt)+"分是B區間";}
+				else if (time  < 420 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到7:00是 C區間,"+"7:00到15:00是B區間,15:00到20:00是A區間,20:00到22:00是B區間,22:00到"+simple.format(nt)+"分是C區間";}	
 			else if (count >=420 && count <  900) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Mid-peak區間";}
 				else if (time  < 900 && count >  900 && count<1200){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到"+simple.format(nt)+"分是A區間";}
-				else if (time  < 900 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到20:00是A區間,20點到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 900 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到20:00是A區間,20點到22點是B區間,22點到"+simple.format(nt)+"分是C區間";}			  
+				else if (time  < 900 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到20:00是A區間,20:00到"+simple.format(nt)+"分是B區間";}
+				else if (time  < 900 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到15:00是 B區間,"+"15:00到20:00是A區間,20:00到22:00是B區間,22:00到"+simple.format(nt)+"分是C區間";}			  
 			else if (count >=900 && count < 1200) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"On -peak區間";}	
 				else if (time  < 1200 && count > 1200 && count<1320){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到20:00是 A區間,"+"20:00到"+simple.format(nt)+"分是B區間";}
-				else if (time  < 1200 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到20:00是 A區間,"+"20:00到22:00是B區間,22點到"+simple.format(nt)+"分是C區間";}
+				else if (time  < 1200 && count > 1320 && count<1440){TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到20:00是 A區間,"+"20:00到22:00是B區間,22:00到"+simple.format(nt)+"分是C區間";}
 			else if (count >=1200 && count< 1320) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Mid-peak區間";}
 				else if (time  < 1320 && count>=1320 &&count<=1440) {TimeRecord[time/60][time%60][hour][min] = startTime+"其中"+simple.format(st)+"分到22:00是 Mid-peak區間,"+"22:00到"+simple.format(nt)+"是Off-peak區間";}
 			else if (count >=1320 && count< 1440) {TimeRecord[time/60][time%60][hour][min] = startTime + nowtime+" 落在"+"Off-peak區間";}
